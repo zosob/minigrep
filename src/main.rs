ff8a09d5a6a85c06c84c09f9dfaa05be93c705ff
Bhaskar Ghosh <bhaskar4647@gmail.com>
2025-06-07 07:57:42 -0500

    Collecting input, and added a panic message for less than 3 arguments.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3{
+        panic!("Error: Expected three arguments, but received {}.", args.len()-1);
+    }
+    println!("Args: {:?}", args);
 }
