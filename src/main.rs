e567d343a68b610f9d63d4ae3fc12d3280e937de
Bhaskar Ghosh <bhaskar4647@gmail.com>
2025-06-07 12:40:17 -0500

    Working searches and used config struct.

--- src/main.rs
+++ src/main.rs
@@ -2,43 +2,87 @@ use std::env;
 use std::fs;
 use std::process;
 
+struct Config{
+    query: String,
+    filename: String,
+    case_insensitive: bool,
+}
+
+impl Config{
+    fn new(args: &[String]) -> Result<Config, &str>{
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        let case_insensitive = std::env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config{
+            query,
+            filename,
+            case_insensitive,
+        })
+    }
+}
+
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 3{
-        panic!("Error: Expected three arguments, but received {}.", args.len()-1);
-    }
-
-    let query = &args[1];
-    let filename = &args[2];
-
-    println!("Searching for: {}", query);
-    println!("In file: {}", filename);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments:{}", err);
+        process::exit(1);
+    });
 
 
-    let contents = match read_file(filename){
-        Ok(c) => c,
-        Err(e) =>{
-            eprintln!("Error reading file: {}", e);
-            process::exit(1);
-        }
-    };
+    let contents = fs::read_to_string(&config.filename).unwrap_or_else(|err| {
+        eprintln!("Error reading file: {}", err);
+        process::exit(1);
+    });
 
     //println!("File contents: \n{}", contents);
 
-    let results = search(query, &contents);
-
+    let results = if config.case_insensitive{
+        search_case_insensitive(&config.query, &contents)
+    } else{
+            search(&config.query, &contents)
+    };
     for line in results{
         println!("{}", line);
     } 
 }
 
-fn read_file(filename: &str) -> Result<String, std::io::Error>{
-    fs::read_to_string(filename)
-}
+//fn read_file(filename: &str) -> Result<String, std::io::Error>{
+//    fs::read_to_string(filename)
+//}
 
 fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
     contents
         .lines()
         .filter(|line| line.contains(query))
         .collect()
+}
+
+fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
+    let query = query.to_lowercase();
+
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
+}
+
+#[test]
+fn case_insensitive(){
+    let query = "rUsT";
+    let contents = "\
+    Rust:
+safe, fast, productive.
+Trust me.";
+
+    assert_eq!(
+        search_case_insensitive(query, contents),
+        vec!["Rust:", "Trust me."]
+    );
+
 }
