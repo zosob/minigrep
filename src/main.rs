973a02f28e969acdc7005eeebfa0e766e0392083
Bhaskar Ghosh <bhaskar4647@gmail.com>
2025-06-07 08:24:49 -0500

    Added file reading, and a new sample text file.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
 use std::env;
+use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3{
         panic!("Error: Expected three arguments, but received {}.", args.len()-1);
     }
-    println!("Args: {:?}", args);
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for: {}", query);
+    println!("In file: {}", filename);
+
+
+    let contents = match read_file(filename){
+        Ok(c) => c,
+        Err{e) =>{
+            eprintln!("Error reading file: {}", e);
+            process::exit(1);
+        }}
+    };
+
+    println!("File contents: \n{}", contents);
+}
+
+fn read_file(filename: &str) -> Result<String, std::io::Error>{
+    fs::read_to_string(filename)
 }
